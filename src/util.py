103654e7e8efa4477196f6ad35415f78123f8400
djacquila <acquila.santos@gmail.com>
2018-10-12 18:36:16 -0300

    Comments

--- src/util.py
+++ src/util.py
@@ -1,5 +1,17 @@
 import socket
 
+# Request
+# GET /index.html HTTP/1.1\r\n
+# Host: www-net.cs.umass.edu\r\n
+# User-Agent: Firefox/3.6.10\r\n
+# Accept: text/html,application/xhtml+xml\r\n
+# Accept-Language: en-us,en;q=0.5\r\n
+# Accept-Encoding: gzip,deflate\r\n
+# Accept-Charset: ISO-8859-1,utf-8;q=0.7\r\n
+# Keep-Alive: 115\r\n
+# Connection: keep-alive\r\n
+# \r\n
+
 def parseHeader(data):
 
 	print data
@@ -16,6 +28,20 @@ def parseHeader(data):
 
 	# retorna o dicionario de dados com a requisicao ja formatada
 	return resultado
+
+# Response
+# HTTP/1.1 200 OK\r\n
+# Date: Sun, 26 Sep 2010 20:09:20 GMT\r\n
+# Server: Apache/2.0.52 (CentOS)\r\n
+# Last-Modified: Tue, 30 Oct 2007 17:00:02 GMT\r\n
+# ETag: "17dc6-a5c-bf716880"\r\n
+# Accept-Ranges: bytes\r\n
+# Content-Length: 2652\r\n
+# Keep-Alive: timeout=10, max=100\r\n
+# Connection: Keep-Alive\r\n
+# Content-Type: text/html; charset=ISO-8859-1\r\n
+# \r\n
+# data data data data data ...
 def parseServerHeader(data):
 	linhas = data.splitlines()
 	primeira_linha = linhas[0].split(' ')
